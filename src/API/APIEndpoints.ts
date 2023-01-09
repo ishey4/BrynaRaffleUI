ea66af117ada37ef7e0abcd6c3763ce9c9b51073
Inbar Gross <ishey4@gmail.com>
2023-01-08 19:44:32 -0500

    adding remove enpoind?

--- src/API/APIEndpoints.ts
+++ src/API/APIEndpoints.ts
@@ -5,4 +5,5 @@ export const GET_PUBLISHABLE_KEY = 'getStripePublishableKey'
 export const CREATE_PAYMENT_INTENT = 'createPaymentIntent'
 export const CEHECK_FOR_RESERVED_TICKETS = "checkForReservedTickets"
 export const GET_TICKET = 'getTicket'
+export const REMOVE_TICKET = 'removeTicket'
 export const CHARGE_CARD = 'chargePayment'
